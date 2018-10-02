1eaa96555cff73a5e8835e0a3c2cfde8bcf8f0d7
Kipngetich33 <khalifngeno@gmail.com>
2018-10-02 18:09:09 +0300

    add functionality to automate tasks operations

--- washmis_erp/washmis_erp/doctype/survey_data/survey_data.js
+++ washmis_erp/washmis_erp/doctype/survey_data/survey_data.js
@@ -3,6 +3,21 @@
 
 frappe.ui.form.on('Survey Data', {
 	refresh: function(frm) {
+		
 
 	}
 });
+
+//function that sets the customer name read_only on save
+// frappe.ui.form.on("Survey Data", "refresh", function(frm) { 
+// 	// use the __islocal value of doc, to check if the doc is saved or not 
+// 	frm.set_df_property("customer_name", "read_only", frm.doc.__islocal ? 0 : 1); 
+// });
+
+// function that returns to task on save 
+frappe.ui.form.on("Survey Data", { after_save: function(frm){
+	console.log("returning")
+	frappe.route_options = {"Priority":"High"}
+	frappe.set_route("Form", "Task","TASK-2018-00030")
+}});
+
