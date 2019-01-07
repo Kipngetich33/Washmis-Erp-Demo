8de70a617f29826b006c941a14a47b23f5c69ca1
Kipngetich <khalifngeno@gmail.com>
2019-01-07 14:10:47 +0300

    add test and functions for Reading Code

--- washmis_erp/washmis_erp/doctype/reading_code/reading_code.py
+++ washmis_erp/washmis_erp/doctype/reading_code/reading_code.py
@@ -7,4 +7,52 @@ import frappe
 from frappe.model.document import Document
 
 class ReadingCode(Document):
-	pass
+	'''
+	Class that test the Reading Code class objects
+	'''
+	def run_functions(self,if_value):
+		'''
+		function that runs all other validation function 
+		of the class reading sheet
+		args:
+			validate function and its arguments
+		}
+		'''
+		if(if_value):
+			pass
+		else:
+			frappe.throw(if_value["message"])
+
+	def validate(self):
+		#check name validity
+		self.run_functions(validate_name(self.name))
+
+
+def validate_name(given_name):
+	'''
+	Function that checks that a name
+	has more than one character
+	args:
+		name
+	output:
+		{"status":True/False, message:"message(only if status is false")
+	'''
+	if(len(given_name)<=1):
+		# frappe.throw("Cannot Save")
+		return {"status":False, "message":"Name must be more than 1 letter"}
+	else:
+		return {"status":True}
+
+def validate_reading_quality(reading_quality_good,reading_quality_bad):
+	'''
+	Function that validates the reading quality
+	to ensure that users click only one (good or bad)
+	'''
+	if(reading_quality_good == 1 and reading_quality_bad == 0):
+		return {"status":True}
+	elif(reading_quality_good == 0 and reading_quality_bad == 1):
+		return {"status":True}
+	else:
+		return {"status":False,"message":"You can only select one Reading Quality (Good or Bad)"}
+	
+
--- washmis_erp/washmis_erp/doctype/reading_code/test_reading_code.py
+++ washmis_erp/washmis_erp/doctype/reading_code/test_reading_code.py
@@ -5,6 +5,79 @@ from __future__ import unicode_literals
 
 import frappe
 import unittest
+import frappe.defaults
+from reading_code import validate_name,validate_reading_quality
+
 
 class TestReadingCode(unittest.TestCase):
-	pass
+	'''
+	Class that tests the Reading Code Doctype 
+	functionality
+	'''
+	def setUp(self):
+		'''
+		function runs at the beggining of every test
+		'''
+		self.test_code = frappe.get_doc({
+				"doctype": "Reading Code",
+				"name1":"test_reading_code",
+			})
+
+	
+	def tearDown(self):
+		'''
+		Destroy the test data after each test
+		'''
+		# frappe.delete_doc("Reading Code","test_reading_code")
+		pass
+
+	def test_validate_name(self):
+		'''
+		Function that tests the validate name
+		function
+		'''
+		self.test_code_1 = frappe.get_doc({
+				"doctype": "Reading Code",
+				"name1":"t",
+			})
+		test_name_1 = validate_name(self.test_code.name1)
+		test_name_2 = validate_name(self.test_code_1.name1)
+		
+		self.assertTrue(test_name_1["status"])
+		self.assertFalse(test_name_2["status"])
+
+	def test_validate_reading_quality(self):
+		'''
+		Function that tests the validate reading quality 
+		function
+		'''
+		self.test_code_1 = frappe.get_doc({
+				"doctype": "Reading Code",
+				"name1":"test_reading_code",
+				"good":1,
+				"bad":0
+			})
+
+		self.test_code_2 = frappe.get_doc({
+				"doctype": "Reading Code",
+				"name1":"test_reading_code",
+				"good":0,
+				"bad":1
+			})
+
+		self.test_code_3 = frappe.get_doc({
+				"doctype": "Reading Code",
+				"name1":"test_reading_code",
+				"good":1,
+				"bad":1
+
+			})
+
+		test_quality_1 = validate_reading_quality(self.test_code_1.good, self.test_code_1.bad)
+		test_quality_2 = validate_reading_quality(self.test_code_2.good, self.test_code_2.bad)
+		test_quality_3 = validate_reading_quality(self.test_code_3.good, self.test_code_3.bad)
+		self.assertTrue(test_quality_1["status"])
+		self.assertTrue(test_quality_2["status"])
+		self.assertFalse(test_quality_3["status"])
+
+
