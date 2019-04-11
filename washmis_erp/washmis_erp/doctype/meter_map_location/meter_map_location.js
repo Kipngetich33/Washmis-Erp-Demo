1d88eebcbb4213a6782c38d42a113444d3f7d189
Kipngetich <khalifngeno@gmail.com>
2019-04-11 11:30:10 +0300

    correct issues with survey data

---
+++ washmis_erp/washmis_erp/doctype/meter_map_location/meter_map_location.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Paul Karugu and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Meter Map Location', {
+	refresh: function(frm) {
+
+	}
+});
